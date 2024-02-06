70ea3b45489ae8dd74dc826d51cabd8b2999f682
trini-tech <trinimarco02@gmail.com>
2024-02-06 12:05:53 +0100

    publicLogofixed

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,4 +1,4 @@
-import ciberLogo from '../../public/ciberlogo.svg';
+import ciberLogo from '../../../public/ciberlogo.svg';
 import { Link } from 'react-router-dom';
 import Button from '../atoms/Button';
 

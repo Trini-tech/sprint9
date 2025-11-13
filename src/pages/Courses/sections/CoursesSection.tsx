58fa538934e02dc8c317f28438cc4477299f05fe
trini-tech <trinimarco02@gmail.com>
2025-11-13 17:19:08 +0100

    CourseSectionchanges

--- src/pages/Courses/sections/CoursesSection.tsx
+++ src/pages/Courses/sections/CoursesSection.tsx
@@ -1,16 +1,20 @@
 import CourseCard from './CourseCard';
 import { CourseProps } from '../../../types/types';
 
-export default function CoursesSection({ courseList }: { courseList: CourseProps[] | null }) {
-  if (courseList === null) {
-    return null;
+interface CoursesSectionProps {
+  courseList: CourseProps[];
+}
+
+export default function CoursesSection({ courseList }: CoursesSectionProps) {
+  if (!courseList || courseList.length === 0) {
+    return <p className="text-center text-gray-500">No courses available</p>;
   }
 
   return (
-    <>
+    <div className="flex flex-col gap-4">
       {courseList.map((course) => (
         <CourseCard key={course.id} course={course} />
       ))}
-    </>
+    </div>
   );
 }
